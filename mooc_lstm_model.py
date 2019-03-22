31b3aad716d4f124911690aa2d11952eabd67594
clarencechen <clarencechenct@gmail.com>
2019-03-22 16:45:20 -0700

    finetuning script

---
+++ mooc_lstm_model.py
@@ -0,0 +1,113 @@
+import datetime
+import json
+import numpy as np
+import keras
+import os
+
+from keras.preprocessing import sequence
+from keras.optimizers import SGD, RMSprop, Adagrad, Adam
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Input
+from keras.models import Model
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, GRU
+from keras.layers.wrappers import TimeDistributed
+import keras.callbacks as callbacks
+
+
+class MOOC_LSTM_Model(object):
+    """
+    
+    """
+    def __init__(self, embedding_vocab_size):
+        """
+        """
+        self.model = None
+        self.model_params = None
+        self.model_histories = []
+        self.embedding_vocab_size = int(embedding_vocab_size) +1
+        self.best_epoch = None
+        self.previous_val_loss = []
+
+    def set_model_name(self, name):
+        if not self.model_params:
+            print("WARNING: Create LSTM model before setting model name.")
+            return -1
+        self.model_name = name + self.model_params_to_string
+
+    @property
+    def model_params_to_string(self):
+        return '_{layers!s}_{seq_len!s}_{embed_dim!s}_{e_vocab_size!s}_{lrate!s}'.format(**self.model_params)
+
+    def create_basic_lstm_model(self, lrate=0.01, opt=RMSprop, layers=2, hidden_size=128, embed_dim=128, seq_len=256, model_load_path=None):
+        """
+        Returns a LSTM model
+        """
+        self.model_params = {'layers': layers,
+            'hidden_size': hidden_size,
+            'embed_dim': embed_dim,
+            'e_vocab_size': self.embedding_vocab_size,
+            'seq_len': seq_len,
+            'lrate': lrate,
+            'optimizer': opt}
+
+        main_input = Input(shape=(seq_len,), name='main_input', dtype='int32')
+        x = Embedding(input_dim=self.embedding_vocab_size, output_dim=embed_dim, input_length=seq_len, mask_zero=True)(main_input)
+        for i in range(layers):
+            print("Adding layer: " + str(i))
+            x = LSTM(hidden_size, dropout=0.2, return_sequences=True)(x)
+        main_loss = TimeDistributed(Dense(self.embedding_vocab_size, activation='softmax'))(x)
+        
+        model = Model(inputs=[main_input], outputs=[main_loss])
+        model.compile(optimizer=opt(lr=lrate), loss='categorical_crossentropy', metrics=['accuracy'])
+
+        # load model weights if specified
+        if model_load_path is not None and os.path.exists(model_load_path):
+            model.load_weights(model_load_path, skip_mismatch=True, by_name=True)
+            load_optimizer_weights(model, model_load_path)
+            print('Old model from {} successfully loaded.\n'.format(model_load_path))
+            model.summary()
+            self.model = model
+            return
+
+        print('Model compiled successfully; will train from new initialization.\n')
+        print('-'*80)
+        model.summary()
+        self.model = model
+
+    def early_stopping_model_fit(self, train_x, train_y, val_x, val_y, epoch_limit=200, loss_nonimprove_limit=3, batch_size=64, model_save_path=None, tensorboard_log_path=None):
+        
+        assert self.model_params is not None
+
+        model_callbacks = []
+        if model_save_path is not None:
+            model_callbacks.append(callbacks.ModelCheckpoint(model_save_path, monitor='val_loss', save_best_only=True, verbose=True))
+        if tensorboard_log_path is not None:
+            model_callbacks.append(callbacks.TensorBoard(tensorboard_log_path, \
+                histogram_freq=1, batch_size=batch_size, write_grads=True, write_images=True))
+
+        early_stopping_met = False
+        for i in range(epoch_limit):
+            print("Epoch: ", i)
+            current_history = self.model.fit(train_x, train_y, validation_data=(val_x, val_y), batch_size=batch_size, epochs=1, callbacks=model_callbacks)
+            current_history = current_history.history
+            validation_loss, validation_accuracy = current_history['val_loss'][0], current_history['val_acc'][0]
+            #average_of_average_accuracy = np.mean(validation_accuracy_dictionary['averages'])
+            self.previous_val_loss.append(validation_loss)
+            if len(self.previous_val_loss) > loss_nonimprove_limit:
+                min_val_loss = min(self.previous_val_loss)
+                recent_losses = self.previous_val_loss[-loss_nonimprove_limit-1:]
+                print(recent_losses)
+                if min(recent_losses) > min_val_loss:
+                    early_stopping_met = True
+                if validation_loss == min_val_loss:
+                    self.best_epoch = i
+                    #self.best_average_of_average_accuracy = average_of_average_accuracy
+                    self.best_accuracy = validation_accuracy
+            if early_stopping_met:
+                print("Early stopping reached.")
+                print("Best epoch according to validation loss:", self.best_epoch)
+                print("Best epoch's accuracy:", self.best_accuracy)
+                #print("Best epoch's average accuracy:", self.best_average_of_average_accuracy)
+                return
--- mooc_transformer_model.py
+++ mooc_transformer_model.py
@@ -4,16 +4,13 @@ import numpy as np
 import keras
 import os
 
+from keras.regularizers import l2 as l2_reg
 from keras.preprocessing import sequence
-from keras.optimizers import SGD, RMSprop, Adagrad, Adam
-from keras.utils import np_utils, multi_gpu_model
-from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers import Input
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.layers import Input, Softmax
 from keras.models import Model
 from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM, GRU
-from keras.layers.wrappers import TimeDistributed
 import keras.callbacks as callbacks
 
 from keras_transformer.extras import ReusableEmbedding, TiedOutputEmbedding
@@ -28,9 +25,9 @@ def build_finetune_model(old_model, model_params, freeze_layers=False):
             layer.trainable = False
 
     # build new finetuning layers
-    reg = regularizers.l2(1e-4)
-    new_course_ids = Input(shape=(model_params['seq_len'],), dtype='int32', name='new_ids')
-    new_embedding = ReusableEmbedding(model_params['e_vocab_size'], model_params['embed_dim'], input_length=model_params['seq_len'], \
+    reg = l2_reg(1e-4)
+    new_course_ids = Input(shape=(int(model_params['seq_len']),), dtype='int32', name='new_ids')
+    new_embedding = ReusableEmbedding(int(model_params['e_vocab_size']), int(model_params['embed_dim']), input_length=int(model_params['seq_len']), \
         name='new_embeddings', embeddings_regularizer=reg)
     
     new_tied_disembedding = TiedOutputEmbedding(projection_regularizer=reg, projection_dropout=0.6, name='new_embedding_logits')
@@ -49,7 +46,7 @@ def build_finetune_model(old_model, model_params, freeze_layers=False):
     finetune_model.summary()
     return finetune_model
 
-class MOOC_Keras_Model(object):
+class MOOC_Transformer_Model(object):
     """
     
     """
@@ -63,13 +60,9 @@ class MOOC_Keras_Model(object):
         self.best_epoch = None
         self.previous_val_loss = []
 
-    def import_data(self, X, y, additional_params = []):
-        """
-        """
-    
     def set_model_name(self, name):
         if not self.model_params:
-            print("WARNING: Create LSTM/Transformer model before setting model name.")
+            print("WARNING: Create Transformer model before setting model name.")
             return -1
         self.model_name = name + self.model_params_to_string
 
@@ -111,7 +104,7 @@ class MOOC_Keras_Model(object):
         model.summary()
         self.model = model
 
-    def crate_finetune_transformer_model(self, model_load_path, old_embedding_size, lrate=2e-3, layers=4, embed_dim=128, seq_len=256, freeze_layers=False):
+    def create_finetune_transformer_model(self, model_load_path, old_embedding_size, lrate=2e-3, layers=4, embed_dim=128, seq_len=256, freeze_layers=False):
         """
         Loads Vanilla Transformer model for finetuning on a new dataset
         """
@@ -123,7 +116,7 @@ class MOOC_Keras_Model(object):
 
         old_model = vanilla_transformer_gpt_model(
                 max_seq_length=seq_len,
-                vocabulary_size=old_embedding_size,
+                vocabulary_size=int(old_embedding_size +1),
                 word_embedding_size=embed_dim,
                 transformer_depth=layers,
                 num_heads=8)
@@ -133,49 +126,13 @@ class MOOC_Keras_Model(object):
         
         # load model weights if specified
         if os.path.exists(model_load_path):
-            model.load_weights(model_load_path, skip_mismatch=True, by_name=True)
+            old_model.load_weights(model_load_path, skip_mismatch=True, by_name=True)
             load_optimizer_weights(old_model, model_load_path)
             print('Old model weights from {} successfully loaded.\n'.format(model_load_path))
         else:
-            raise FileNotFoundError, 'Please specify a valid model_load_path to load pretrained weights.'
+            raise FileNotFoundError('Please specify a valid model_load_path to load pretrained weights.')
 
         self.model = build_finetune_model(old_model, self.model_params, freeze_layers)
-
-    def create_basic_lstm_model(self, lrate=0.01, opt=Adagrad, layers=2, hidden_size=128, embed_dim=128, seq_len=256, model_load_path=None):
-        """
-        Returns a LSTM model
-        """
-        self.model_params = {'layers': layers,
-            'hidden_size': hidden_size,
-            'embed_dim': embed_dim,
-            'e_vocab_size': self.embedding_vocab_size,
-            'seq_len': seq_len,
-            'lrate': lrate,
-            'optimizer': opt}
-
-        main_input = Input(shape=(seq_len,), name='main_input', dtype='int32')
-        x = Embedding(input_dim=self.embedding_vocab_size, output_dim=embed_dim, input_length=seq_len, mask_zero=True)(main_input)
-        for i in range(layers):
-            print("Adding layer: " + str(i))
-            x = LSTM(hidden_size, dropout=0.2, return_sequences=True)(x)
-        main_loss = TimeDistributed(Dense(self.embedding_vocab_size, activation='softmax'))(x)
-        
-        model = Model(inputs=[main_input], outputs=[main_loss])
-        model.compile(optimizer=opt(lr=lrate), loss='categorical_crossentropy', metrics=['accuracy'])
-
-        # load model weights if specified
-        if model_load_path is not None and os.path.exists(model_load_path):
-            model.load_weights(model_load_path, skip_mismatch=True, by_name=True)
-            load_optimizer_weights(model, model_load_path)
-            print('Old model from {} successfully loaded.\n'.format(model_load_path))
-            model.summary()
-            self.model = model
-            return
-
-        print('Model compiled successfully; will train from new initialization.\n')
-        print('-'*80)
-        model.summary()
-        self.model = model
 
     def transformer_model_fit(self, train_x, train_y, val_x, val_y, epoch_limit=5, batch_size=64, model_save_path=None, tensorboard_log_path=None):
 
@@ -210,41 +167,6 @@ class MOOC_Keras_Model(object):
         test_metrics = self.model.evaluate(test_x, test_y, batch_size=batch_size)
         for metric_name, metric_value in zip(self.model.metrics_names, test_metrics):
             print('Test {}: {:.8f}'.format(metric_name, metric_value))
-
-    def early_stopping_model_fit(self, train_x, train_y, val_x, val_y, epoch_limit=200, loss_nonimprove_limit=3, batch_size=64, model_save_path=None, tensorboard_log_path=None):
-        
-        assert self.model_params is not None
-
-        model_callbacks = []
-        if model_save_path is not None:
-            model_callbacks.append(callbacks.ModelCheckpoint(model_save_path, monitor='val_loss', save_best_only=True, verbose=True))
-        if tensorboard_log_path is not None:
-            model_callbacks.append(callbacks.TensorBoard(tensorboard_log_path))
-
-        early_stopping_met = False
-        for i in range(epoch_limit):
-            print("Epoch: ", i)
-            current_history = self.model.fit(train_x, train_y, validation_data=(val_x, val_y), batch_size=batch_size, epochs=1)
-            current_history = current_history.history
-            validation_loss, validation_accuracy = current_history['val_loss'][0], current_history['val_acc'][0]
-            #average_of_average_accuracy = np.mean(validation_accuracy_dictionary['averages'])
-            self.previous_val_loss.append(validation_loss)
-            if len(self.previous_val_loss) > loss_nonimprove_limit:
-                min_val_loss = min(self.previous_val_loss)
-                recent_losses = self.previous_val_loss[-loss_nonimprove_limit-1:]
-                print(recent_losses)
-                if min(recent_losses) > min_val_loss:
-                    early_stopping_met = True
-                if validation_loss == min_val_loss:
-                    self.best_epoch = i
-                    #self.best_average_of_average_accuracy = average_of_average_accuracy
-                    self.best_accuracy = validation_accuracy
-            if early_stopping_met:
-                print("Early stopping reached.")
-                print("Best epoch according to validation loss:", self.best_epoch)
-                print("Best epoch's accuracy:", self.best_accuracy)
-                #print("Best epoch's average accuracy:", self.best_average_of_average_accuracy)
-                return
 
 """
     def simple_model_fit(self, epochs = 10, batch_size = 64, validation_proportion = 0.1):
